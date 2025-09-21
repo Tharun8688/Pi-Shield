3da8765067e1b3042347a6cfcb2da593b6215e78
LANKIPALLI THARUN <tharun393325@gmail.com>
2025-09-21 12:33:05 +0530

    fourteen commit

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Pi-Shield 🚀");
-});
+// Serve frontend files
+app.use(express.static(path.join(__dirname, "dist")));
 
 import apiRoutes from './routes';
 
 app.use(apiRoutes);
+
+// Fallback for SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
